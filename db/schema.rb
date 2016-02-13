eca20415c32afc2bb7e91c38070a3085bb135ee8
Whitley Blaine <whitleyblaine@gmail.com>
2016-02-13 14:03:25 -0500

    Reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 20160213002219) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
