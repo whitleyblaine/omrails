0432914222247a04b1da1796ca07c583c7bd75e5
Whitley Blaine <whitleyblaine@gmail.com>
2016-02-17 22:42:31 -0500

    Made s3 info more secure in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => "picturesque1",
-    :access_key_id => "AKIAIKLCEIJKJUO44KEQ",
-    :secret_access_key => "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"
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
